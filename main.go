c34e9f0979594c6333e2061f5ede4c7e149758b2
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-12 23:01:45 +0200

    Add fiber dependency

--- main.go
+++ main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"log"
 
+	"github.com/gofiber/fiber/v2"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	log.Println("Go rest api")
+
+	app := fiber.New()
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Up and running",
+		})
+	})
+
+	app.Listen(":3000")
 }
