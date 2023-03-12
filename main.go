e2d9e46fea6e925e022a17dfa53b58389a8ebe7b
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-12 23:04:22 +0200

    Configure JSON error handler

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"errors"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// new JSON error handler
+func errorHandler(c *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+
+	// retrieve status code if it is a fiber error
+	var e *fiber.Error
+	if errors.As(err, &e) {
+		code = e.Code
+	}
+
+	log.Printf("ERROR: %s\n", err.Error())
+	return c.Status(code).JSON(fiber.Map{"message": err.Error()})
+}
+
 func main() {
 	log.Println("Go rest api")
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{ErrorHandler: errorHandler})
 
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
